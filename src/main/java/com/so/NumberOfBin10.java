adb40f5050e52ed49005091c034cbe49515d6bc0
GaoLeiQin <1031819389@qq.com>
2019-02-24 16:39:26 +0800

    第10题
    输入一个整数，输出该数二进制表示中1的个数。

---
+++ src/main/java/com/so/NumberOfBin10.java
@@ -0,0 +1,26 @@
+package com.so;
+
+/**
+ * 第10题
+ * 输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
+ *
+ * @author qgl
+ * @date 2017/08/09
+ */
+public class NumberOfBin10 {
+
+    /**
+     * 思路：把一个整数减去１再和原整数与，就会把整数最右边一个１变成０
+     * @param n
+     * @return
+     */
+    public static int numberOfBin(int n) {
+        int count=0;
+
+        while(n!=0) {
+            count++;
+            n=(n-1) & n;
+        }
+        return count;
+    }
+}
---
+++ src/test/java/com/so/Test10.java
@@ -0,0 +1,18 @@
+package com.so;
+
+import org.junit.Test;
+
+/**
+ * 第10题
+ * 输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
+ *
+ * @author qgl
+ * @date 2019/02/24
+ */
+public class Test10 {
+    @Test
+    public void test9() throws Exception {
+        int number = 7;
+        System.out.println(number + " 的二进制表示中1的个数：" + NumberOfBin10.numberOfBin(number));
+    }
+}
