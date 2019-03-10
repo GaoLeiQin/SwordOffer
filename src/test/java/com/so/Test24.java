703818f83dc99705b4950563055fe4b427133024
GaoLeiQin <1031819389@qq.com>
2019-03-10 09:29:43 +0800

    第24题
    输入一个数组，判断数组是不是二叉搜索树的后序遍历结果

---
+++ src/test/java/com/so/Test24.java
@@ -0,0 +1,18 @@
+package com.so;
+
+import org.junit.Test;
+
+/**
+ * 第24题
+ * 输入一个数组，判断数组是不是二叉搜索树的后序遍历结果
+ *
+ * @author qgl
+ * @date 2019/03/10
+ */
+public class Test24 {
+    @Test
+    public void test24() throws Exception {
+        int[] array = {4, 8, 6, 12, 16, 14, 10};
+        System.out.println("结果：" + VerifySequerceOfBST24.verifySequenceOfBST(array));
+    }
+}
