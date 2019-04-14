c46bd0db9554fc5b452d59c12205ffd3c33b2c7e
GaoLeiQin <1031819389@qq.com>
2019-04-14 09:34:51 +0800

    格式化代码
    修正第19题测试用例代码风格

--- src/test/java/com/so/Test19.java
+++ src/test/java/com/so/Test19.java
@@ -12,26 +12,26 @@ import org.junit.Test;
 public class Test19 {
     @Test
     public void test19() throws Exception {
-        MirrorRecursively19.BinaryTreeNode root1=new MirrorRecursively19.BinaryTreeNode();
-        MirrorRecursively19.BinaryTreeNode node1=new MirrorRecursively19.BinaryTreeNode();
-        MirrorRecursively19.BinaryTreeNode node2=new MirrorRecursively19.BinaryTreeNode();
-        MirrorRecursively19.BinaryTreeNode node3=new MirrorRecursively19.BinaryTreeNode();
-        MirrorRecursively19.BinaryTreeNode node4=new MirrorRecursively19.BinaryTreeNode();
-        MirrorRecursively19.BinaryTreeNode node5=new MirrorRecursively19.BinaryTreeNode();
-        MirrorRecursively19.BinaryTreeNode node6=new MirrorRecursively19.BinaryTreeNode();
-        root1.leftNode=node1;
-        root1.rightNode=node2;
-        node1.leftNode=node3;
-        node1.rightNode=node4;
-        node4.leftNode=node5;
-        node4.rightNode=node6;
-        root1.data=8;
-        node1.data=5;
-        node2.data=7;
-        node3.data=9;
-        node4.data=2;
-        node5.data=4;
-        node6.data=1;
+        MirrorRecursively19.BinaryTreeNode root1 = new MirrorRecursively19.BinaryTreeNode();
+        MirrorRecursively19.BinaryTreeNode node1 = new MirrorRecursively19.BinaryTreeNode();
+        MirrorRecursively19.BinaryTreeNode node2 = new MirrorRecursively19.BinaryTreeNode();
+        MirrorRecursively19.BinaryTreeNode node3 = new MirrorRecursively19.BinaryTreeNode();
+        MirrorRecursively19.BinaryTreeNode node4 = new MirrorRecursively19.BinaryTreeNode();
+        MirrorRecursively19.BinaryTreeNode node5 = new MirrorRecursively19.BinaryTreeNode();
+        MirrorRecursively19.BinaryTreeNode node6 = new MirrorRecursively19.BinaryTreeNode();
+        root1.leftNode = node1;
+        root1.rightNode = node2;
+        node1.leftNode = node3;
+        node1.rightNode = node4;
+        node4.leftNode = node5;
+        node4.rightNode = node6;
+        root1.data = 8;
+        node1.data = 5;
+        node2.data = 7;
+        node3.data = 9;
+        node4.data = 2;
+        node5.data = 4;
+        node6.data = 1;
 /*
               原二叉树          二叉树的镜像
                  8                   8
