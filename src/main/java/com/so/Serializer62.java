5d5b7c748c3035851c526551e854ba8e97011580
GaoLeiQin <1031819389@qq.com>
2019-04-18 08:54:05 +0800

    第62题
    二叉树的序列化与反序列化

---
+++ src/main/java/com/so/Serializer62.java
@@ -0,0 +1,58 @@
+package com.so;
+
+import com.so.LevelPrintTree61.TreeNode;
+
+/**
+ * 第62题
+ * 二叉树的序列化与反序列化
+ *
+ * @author qgl
+ * @date 2017/08/30
+ */
+public class Serializer62 {
+
+    /**
+     * 序列化二叉树 (前序遍历)
+     *
+     * @param root
+     * @return
+     */
+    public static String serialize(TreeNode root) {
+        StringBuffer sb = new StringBuffer();
+        if (root == null) {
+            sb.append("#,");
+            return sb.toString();
+        }
+
+        sb.append(root.val + ",");
+        sb.append(serialize(root.left));
+        sb.append(serialize(root.right));
+        return sb.toString();
+    }
+
+    private static int index = -1;
+
+    /**
+     * 反序列化二叉树
+     *
+     * @param str
+     * @return
+     */
+    public static TreeNode deserialize(String str) {
+        index++;
+        int len = str.length();
+        String[] strArray = str.split(",");
+        TreeNode node = null;
+
+        if (index >= len) {
+            return null;
+        }
+
+        if (!strArray[index].equals("#")) {
+            node = new TreeNode(Integer.valueOf(strArray[index]));
+            node.left = deserialize(str);
+            node.right = deserialize(str);
+        }
+        return node;
+    }
+}
---
+++ src/test/java/com/so/Test62.java
@@ -0,0 +1,35 @@
+package com.so;
+
+import com.so.LevelPrintTree61.TreeNode;
+import org.junit.Test;
+
+/**
+ * 第62题
+ * 二叉树的序列化与反序列化
+ *
+ * @author qgl
+ * @date 2019/04/18
+ */
+public class Test62 {
+
+    @Test
+    public void test62() throws Exception {
+        TreeNode root = new TreeNode(7);
+        TreeNode node1 = new TreeNode(5);
+        TreeNode node2 = new TreeNode(8);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(6);
+        TreeNode node5 = new TreeNode(9);
+        TreeNode node6 = new TreeNode(11);
+        root.left = node1;
+        root.right = node2;
+        node1.left = node3;
+        node1.right = node4;
+        node2.left = node5;
+        node2.right = node6;
+
+        String serializerTree = Serializer62.serialize(root);
+        System.out.println("序列化二叉树：" + serializerTree);
+        System.out.println("反序列化二叉树后，从左至右输出：" + LevelPrintTree61.printTreeNodeByLeftToRight(Serializer62.deserialize(serializerTree)));
+    }
+}
