20612330e6710a6b783d2cec812fce0905cad070
GaoLeiQin <1031819389@qq.com>
2019-07-02 19:58:38 +0800

    第5题
    从尾到头打印链表，修改解法

--- src/test/java/com/so/Test5.java
+++ src/test/java/com/so/Test5.java
@@ -1,12 +1,10 @@
 package com.so;
 
+import com.so.Common.ListNode;
 import org.junit.Test;
-import com.so.PrintListReversing5.ListNode;
-
-import java.util.Arrays;
 
 /**
- * 第5题
+ * 第5题 从尾到头打印链表
  * 输入一个链表，从尾到头打印链表每个节点的值
  *
  * @author qgl
@@ -15,17 +13,12 @@ import java.util.Arrays;
 public class Test5 {
     @Test
     public void test5() {
-        int[] beforeList = {3, 6, 9};
-        ListNode node1 = new ListNode();
-        ListNode node2 = new ListNode();
-        ListNode node3 = new ListNode();
-        node1.val = beforeList[0];
-        node2.val = beforeList[1];
-        node3.val = beforeList[2];
+        ListNode node1 = new ListNode(3);
+        ListNode node2 = new ListNode(6);
+        ListNode node3 = new ListNode(9);
         node1.next = node2;
         node2.next = node3;
-        System.out.println("解法一：利用栈输出，    原链表：" + Arrays.toString(beforeList) + " 反转后：" + PrintListReversing5.printListReverse1(node1));
-        System.out.println("解法一：借助栈存入数组，原链表：" + Arrays.toString(beforeList) + " 反转后：" + PrintListReversing5.printListReverse2(node1));
-        System.out.println("解法一：递归，          原链表：" + Arrays.toString(beforeList) + " 反转后：" + PrintListReversing5.printListReverse3(node1));
+        System.out.println("解法一：利用栈输出，反转后：" + PrintListReversing5.printListReverse1(node1));
+        System.out.println("解法二：递归，反转后：" + PrintListReversing5.printListReverse2(node1));
     }
 }
