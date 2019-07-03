2a92a90aca4d8d55cccbcd09b4fa9afae1721377
GaoLeiQin <1031819389@qq.com>
2019-07-03 11:04:10 +0800

    第25题
    优化解法

--- src/main/java/com/so/FindPath25.java
+++ src/main/java/com/so/FindPath25.java
@@ -2,6 +2,8 @@ package com.so;
 
 import java.util.*;
 
+import com.so.Common.TreeNode;
+
 /**
  * 第25题
  * 输入一个二叉树和一个整数，打印出二叉树中节点值的和为输入整数的所有路径
@@ -10,76 +12,31 @@ import java.util.*;
  * @date 2017/08/13
  */
 public class FindPath25 {
-    /**
-     * 二叉树
-     */
-    static class BinaryTreeNode {
-        int data;
-        BinaryTreeNode leftNode;
-        BinaryTreeNode rightNode;
-    }
-
-    /**
-     * 利用栈寻找全路径
-     * @param root
-     * @param k
-     */
-    public static void findAllPath1(BinaryTreeNode root, int k) {
-        if (root == null) {
-            return;
-        }
-        Stack<Integer> stack = new Stack<Integer>();
-        findPath(root, k, stack);
-    }
-
-    private static void findPath(BinaryTreeNode root, int k, Stack<Integer> path) {
-        if (root == null) {
-            return;
-        }
-
-        if (root.leftNode == null && root.rightNode == null) {
-            if (root.data == k) {
-                System.out.print("路径开始:");
-                for (int a : path) {
-                    System.out.print(a + ",");
-                }
-                //最后输出叶子节点
-                System.out.println(root.data);
-            }
-        } else {
-            path.push(root.data);
-            findPath(root.leftNode, k - root.data, path);
-            findPath(root.rightNode, k - root.data, path);
-            path.pop();
-        }
-    }
-
-    static ArrayList<ArrayList<Integer>> resultList = new ArrayList<>();
-    static ArrayList<Integer> list = new ArrayList<>();
+    private ArrayList<ArrayList<Integer>> listAll = new ArrayList<>();
+    private ArrayList<Integer> list = new ArrayList<>();
 
     /**
      * 利用list寻找全路径
+     *
      * @param root
      * @param target
      * @return
      */
-    public static ArrayList<ArrayList<Integer>> findAllPath2(BinaryTreeNode root, int target) {
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
         if (root == null) {
-            return resultList;
+            return listAll;
         }
-        list.add(root.data);
-        target -= root.data;
+        list.add(root.val);
+        target -= root.val;
 
-        if (target == 0 && root.leftNode == null && root.rightNode == null) {
-            resultList.add(new ArrayList<>(list));
-        } else {
-            findAllPath2(root.leftNode, target);
-            findAllPath2(root.rightNode, target);
+        if (target == 0 && root.left == null && root.right == null) {
+            listAll.add(new ArrayList<>(list));
         }
-        //每次返回上一层要回退一个节点
+        FindPath(root.left, target);
+        FindPath(root.right, target);
+        // 回退
         list.remove(list.size() - 1);
-        return resultList;
+        return listAll;
     }
-
 }
 
