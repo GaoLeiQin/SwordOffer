e3b7d1aba4d90298b22303e03cb6a23ecbafeb7b
GaoLeiQin <1031819389@qq.com>
2019-07-18 23:03:12 +0800

    第30题
    优化解法

--- src/main/java/com/so/GetLeastNumbers30.java
+++ src/main/java/com/so/GetLeastNumbers30.java
@@ -14,25 +14,28 @@ public class GetLeastNumbers30 {
 
     /**
      * 获取最小的k个数
+     *
      * @param array
      * @param k
      * @return
      */
     public static ArrayList<Integer> getLeastNumbers(int[] array, int k) {
         ArrayList<Integer> list = new ArrayList<>();
-        if (array == null || k < 0 || k > array.length) {
-            return null;
+        if (array == null || k < 1 || k > array.length) {
+            return list;
         }
         int[] kArray = Arrays.copyOfRange(array, 0, k);
 
         //根据输入数组前k个数建立最大堆
-        buildMaxHeapify(kArray);
+        for (int i = kArray.length / 2; i >= 0; i--) {
+            maxHeapIfy(kArray, i, kArray.length);
+        }
 
         //从k开始与根节点比较，若大于根节点，舍去，若小于，取代根节点，重排最大堆
         for (int i = k; i < array.length; i++) {
             if (array[i] < kArray[0]) {
                 kArray[0] = array[i];
-                maxHeapIfy(kArray, 0);
+                maxHeapIfy(kArray, 0, kArray.length);
             }
         }
 
@@ -44,42 +47,33 @@ public class GetLeastNumbers30 {
     }
 
     /**
-     * 保持堆的性质
+     * 调整大根堆
+     *
      * @param array
-     * @param i
+     * @param root
+     * @param length
      */
-    private static void maxHeapIfy(int[] array, int i) {
-        int left = 2 * i + 1;
-        int right = left + 1;
-        int largest = 0;
+    private static void maxHeapIfy(int[] array, int root, int length) {
+        int left = 2 * root + 1;
 
-        if (left < array.length && array[left] > array[i]) {
-            largest = left;
-        } else {
-            largest = i;
+        if (left >= length) {
+            return;
         }
 
-        if (right < array.length && array[right] > array[largest]) {
-            largest = right;
+        if (left + 1 < length && array[left] < array[left + 1]) {
+            left++;
         }
 
-        if (largest != i) {
-            int temp = array[i];
-            array[i] = array[largest];
-            array[largest] = temp;
-            maxHeapIfy(array, largest);
+        if (array[left] > array[root]) {
+            swap(array, left, root);
+            maxHeapIfy(array, left, length);
         }
     }
 
-    /**
-     * 构建大根堆
-     * @param array
-     */
-    private static void buildMaxHeapify(int[] array) {
-        //找到第一个非叶子节点
-        for (int i = array.length / 2; i >= 0; i--) {
-            maxHeapIfy(array, i);
-        }
+    private static void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
     }
 
 }
